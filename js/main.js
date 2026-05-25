073d827d154c4eb3dc61852f0f43ccd4b48b57bd
Ramon Alencar <ramonalencarandrade@gmail.com>
2026-05-25 14:36:04 -0300

    Feito o adicionamento do inpute voltar ao topo

--- js/main.js
+++ js/main.js
@@ -161,3 +161,38 @@ menuLinks.forEach(link => {
   });
 
 });
+
+/* BACK TO TOP */
+
+const backToTop =
+  document.querySelector('.back-to-top');
+
+window.addEventListener('scroll', () => {
+
+  if(window.scrollY > 500){
+
+    backToTop.classList.add('show');
+
+  }else{
+
+    backToTop.classList.remove('show');
+
+  }
+
+});
+
+backToTop.addEventListener('click', () => {
+
+  window.scrollTo({
+
+    top: 0,
+
+    behavior: 'smooth'
+
+  });
+
+});
+
+/* LUCIDE ICONS */
+
+lucide.createIcons();
